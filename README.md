e8b307f30ec3ad88e166657dd0e3409c9b40f6d3
Chrystian Huot <chrystian.huot@saubeo.solutions>
2020-11-30 08:56:51 -0500

    Initial opensource commit

---
+++ README.md
@@ -0,0 +1,115 @@
+<h1 align="center">
+    Panda Timer
+</h1>
+
+<p align="center">
+    <img src="panda-timer.png" alt="Panda Timer">
+</p>
+
+This javascript widget was created for needs of the [Association Panda Thérèse-de Blainville et des Laurentides](https://translate.google.com/translate?sl=fr&tl=en&u=pandatdb.com) in order to contribute to the promotion of its services to the population by making this usefull tool available.
+
+**Panda** stands for ***P**ersonne **A**pte à **N**égocier avec le **D**éficit d’**A**ttention* in French, which can be translated to *Person Able to Negotiate with Attention Deficit Disorder*.
+
+> Note that if you find this widget useful, consider supporting your local ADHD organization.
+
+## Features
+
+- It's a javascript countdown widget;
+- It emits a single quiet beep every 55, 50, 45, 45 ... remaining minutes;
+- It emits two loud beeps when there is no more time;
+- It's fully configurable;
+- It just works.
+
+## Quick start
+
+Just copy the `dist / panda-timer.min.js` file somewhere on your website, then add this `HTML` code to your webpage:
+
+```html
+<canvas id="panda-timer" height="500" width="500"></canvas>
+<script src="panda-timer.min.js"></script>
+<script>
+  new PandaTimer();
+</script>
+```
+
+## API
+
+```javascript
+// Configurable parameters, see below.
+const config = {};
+
+// Get the canvas element to paint on, by default it searches for 'canvas#panda-timer' if not specified.
+const canvasElement = document.getElementById('panda-timer');
+
+// Instanciate a new Panda Timer.
+const pandaTimer = new PandaTimer(config, canvasElement);
+```
+
+```javascript
+// Set the remaining time in seconds without starting the timer.
+pandaTimer.setTimeLeft(300);
+```
+
+```javascript
+// Start the timer without setting the remaining time.
+pandaTimer.start();
+
+// Start the timer at 300 seconds.
+pandaTimer.start(300);
+```
+
+```javascript
+// Pause the timer.
+pandaTimer.stop();
+```
+
+```javascript
+// Subscribe to the timer and call the callback function when 300 seconds left.
+const subscription = pandaTimer.subscribe((timeLeft) => console.log(timeLeft), 300);
+
+// Unsubscribe from the timer subscription.
+subscription.unsubscribe();
+```
+
+## Default configuration
+
+```javascript
+{
+    alterable: true,                  // Allow users to change the remaining
+                                      // time by rotating the cursor around
+                                      // the clock face.
+    autostart: true,                  // Start the timer immediately or not.
+    color: {
+        background: '#fff',           // Background color of the canvas.
+        cursor: 'rgba(0, 0, 0, 0.3)', // Color of the cursor.
+        face: '#fff',                 // Color of the rounded face.
+        panda: '#333',                // Panda color.
+        scale: '#333',                // Scale color.
+        text: '#333',                 // Text line color.
+        timer: '#f00',                // Timer color.
+    },
+    font: 'arial',                    // Text font.
+    text: {
+        line1: 'Panda Timer',         // First text line.
+        line1: '',                    // Second text line.
+    },
+    timeLeft: 0,                      // Start with the time remaining in seconds.
+    timeMax: 3600,                    // Maximum timer time in seconds.
+}
+```
+
+## Responsive website
+
+[Panda Timer](https://github.com/chuot/panda-timer) can be responsive by simply not specifying the `height` and `width` attributes on the canvas element.
+
+The widget will then use the size of the `parent element` to size the canvas accordingly. This is useful for wrapping the widget in a `flex container`. The widget will resize when the window is resized.
+
+```html
+<canvas id="panda-timer"></canvas>
+```
+
+## Show your appreciation / Support the author
+
+If you like [Panda Timer](https://github.com/chuot/panda-timer) please consider **starring the repository** to show you appreciation to the author for his hard work. It cost nothing but is really appreciated.
+
+If you use [Panda Timer](https://github.com/chuot/panda-timer) for commercial purposes or derive income from it, please consider [sponsoring this project](https://github.com/sponsors/chuot) to help support continued development.
---
+++ src/panda-timer.js
@@ -0,0 +1,581 @@
+/*
+ * *****************************************************************************
+ * @license
+ * Panda Timer <https://github.com/chuot/panda-timer>
+ * Copyright (C) 2020 Chrystian Huot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
+ */
+
+class PandaTimer {
+    get _centerX() {
+        return this._canvas.width / 2;
+    }
+
+    get _centerY() {
+        return this._canvas.height / 2;
+    }
+
+    get _height() {
+        return this._canvas.height;
+    }
+
+    get _radius() {
+        return Math.floor(Math.min(this._canvas.height, this._canvas.width) / 2 * 0.9);
+    }
+
+    get _timeLeft() {
+        return this.__timeLeft;
+    }
+    set _timeLeft(timeLeft) {
+        this.__timeLeft = Math.max(0, Math.min(this._config.timeMax, parseInt(`${timeLeft}`) || 0));
+    }
+
+    get _width() {
+        return this._canvas.width;
+    }
+
+    constructor(config = {}, canvas = document.getElementById('panda-timer')) {
+        if (!(canvas instanceof HTMLCanvasElement)) {
+            console.warn('canvas#panda-timer not found');
+
+            return {};
+        }
+
+        this._audioContext = null;
+
+        this._canvas = canvas;
+
+        config = config !== null && typeof config === 'object' ? config : {};
+        config.color = config.color !== null && typeof config.color === 'object' ? config.color : {};
+        config.text = config.text !== null && typeof config.text === 'object' ? config.text : {};
+
+        const timeLeft = parseInt(`${config.timeLeft}`);
+
+        this._config = {
+            alterable: typeof config.alterable === 'boolean' ? config.alterable : PandaTimer.defaultConfig.alterable,
+            autostart: typeof config.autostart === 'boolean' ? config.autostart : PandaTimer.defaultConfig.autostart,
+            color: {
+                background: typeof config.color.background === 'string' ? config.color.background : PandaTimer.defaultConfig.color.background,
+                cursor: typeof config.color.cursor === 'string' ? config.color.cursor : PandaTimer.defaultConfig.color.cursor,
+                face: typeof config.color.face === 'string' ? config.color.face : PandaTimer.defaultConfig.color.face,
+                panda: typeof config.color.panda === 'string' ? config.color.panda : PandaTimer.defaultConfig.color.panda,
+                scale: typeof config.color.scale === 'string' ? config.color.scale : PandaTimer.defaultConfig.color.scale,
+                text: typeof config.color.text === 'string' ? config.color.text : PandaTimer.defaultConfig.color.text,
+                timer: typeof config.color.timer === 'string' ? config.color.timer : PandaTimer.defaultConfig.color.timer,
+            },
+            font: config.font || PandaTimer.defaultConfig.font,
+            text: {
+                line1: config.text.line1 || PandaTimer.defaultConfig.text.line1,
+                line2: config.text.line2 || PandaTimer.defaultConfig.text.line2,
+            },
+            timeLeft: isNaN(timeLeft) ? PandaTimer.defaultConfig.timeLeft : timeLeft,
+            timeMax: parseInt(`${config.timeMax}`) || PandaTimer.defaultConfig.timeMax,
+        };
+
+        this._ctx = canvas && canvas.getContext('2d');
+
+        this._cursorMoving = false;
+
+        this._cursorPrevious = null;
+
+        this._intervalHandle = null;
+
+        this._lastTime = null;
+
+        this._responsive = !this._canvas.attributes.getNamedItem('height') && !this._canvas.attributes.getNamedItem('width');
+
+        this._subscriptions = [];
+
+        this.__timeLeft = 0;
+
+        this._timeLeft = this._config.timeLeft;
+
+        this._bootstrap();
+
+        if (this._responsive) {
+            this._resize();
+
+            window.addEventListener('resize', () => {
+                this._resize();
+
+                this._draw();
+            });
+        }
+
+        this._draw();
+
+        if (this._config.alterable) {
+            ['mousedown', 'touchstart'].forEach((type) => this._canvas.addEventListener(type, (event) => this._onTouch(event)));
+            ['mousemove', 'touchmove'].forEach((type) => this._canvas.addEventListener(type, (event) => this._onMove(event)));
+            ['mouseleave', 'mouseup', 'touchend'].forEach((type) => this._canvas.addEventListener(type, () => this._onRelease()));
+        }
+
+        document.addEventListener('visibilitychange', () => {
+            if (document.visibilityState === 'visible') {
+                if (this.intervalHandle) {
+                    this.start();
+
+                } else {
+                    this._draw();
+                }
+
+                if (this._audioContext) {
+                    setTimeout(() => this._audioContext.resume(), 100);
+                }
+            }
+        });
+
+        window.addEventListener('beforeunload', (event) => {
+            if (this._intervalHandle) {
+                event.preventDefault();
+
+                event.returnValue = '';
+            }
+        });
+
+        if (this._config.autostart) {
+            this.start();
+        }
+    }
+
+    setTimeLeft(timeLeft) {
+        this._timeLeft = timeLeft;
+
+        this._draw();
+    }
+
+    start(timeLeft = this._timeLeft) {
+        if (!timeLeft) {
+            return;
+
+        } else if (timeLeft !== this._timeLeft) {
+            this._timeLeft = timeLeft;
+
+            this._draw();
+        }
+
+        const interval = document.visibilityState === 'visible' ? 1 : timeLeft;
+
+        if (this._intervalHandle) {
+            clearInterval(this._intervalHandle);
+
+            this._tick();
+
+            this._draw();
+        }
+
+        this._lastTime = new Date();
+
+        this._intervalHandle = setInterval(() => {
+            this._tick();
+
+            this._lastTime = new Date();
+
+            if (this._timeLeft < 1) {
+                clearInterval(this._intervalHandle);
+
+                this._intervalHandle = null;
+
+                this._timeLeft = 0;
+
+                this._playCompleted();
+
+            } else if (!(Math.round(this._timeLeft) % 300)) {
+                this._playReminder();
+            }
+
+            this._draw();
+        }, interval * 1000);
+    }
+
+    stop() {
+        if (this._intervalHandle) {
+            clearInterval(this._intervalHandle);
+
+            this._intervalHandle = null;
+        }
+    }
+
+    subscribe(callback, timeLeft = 0) {
+        const roundedTimeLeft = Math.round(timeLeft);
+
+        const subscription = {
+            unsubscribe: () => {
+                const index = this._subscriptions.find((_subscription) => _subscription === subscription);
+
+                if (index !== -1) {
+                    this._subscriptions.splice(index, 1);
+
+                    return true;
+
+                } else {
+                    return false;
+                }
+            },
+            get _callback() { return typeof callback === 'function' ? callback : () => { } },
+            get _timeLeft() { return roundedTimeLeft },
+        };
+
+        this._subscriptions.push(subscription);
+
+        return subscription;
+    }
+
+    _bootstrap() {
+        const events = ['keydown', 'mousedown', 'touchstart'];
+
+        const bootstrap = async () => {
+            if (!this._audioContext) {
+                this._audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            }
+
+            if (this._audioContext) {
+                this._audioContext.resume();
+
+                events.forEach((event) => document.body.removeEventListener(event, bootstrap));
+            }
+        }
+
+        events.forEach((event) => document.body.addEventListener(event, bootstrap));
+    }
+
+    _draw() {
+        this._drawBackground();
+        this._drawFace();
+        this._drawTicks();
+        this._drawTimeLeft();
+        this._drawPanda();
+
+        if (this._config.alterable) {
+            this._drawCursor();
+        }
+
+        this._drawNumbers();
+        this._drawText();
+    }
+
+    _drawBackground() {
+        this._ctx.rect(0, 0, this._width, this._height);
+        this._ctx.fillStyle = this._config.color.background;
+        this._ctx.fill();
+    }
+
+    _drawCursor() {
+        const angle = 2 * Math.PI / this._config.timeMax * (this._config.timeMax - this._timeLeft);
+
+        this._ctx.beginPath();
+        this._ctx.translate(this._centerX + this._radius * 0.8 * Math.sin(angle), this._centerY + this._radius * -0.8 * Math.cos(angle));
+        this._ctx.moveTo(0, 0);
+        this._ctx.rotate(angle);
+        this._ctx.lineTo(this._radius * -0.1, this._radius * -0.1);
+        this._ctx.arcTo(this._radius * -0.1, this._radius * -0.2, 0, this._radius * -0.2, this._radius * 0.1);
+        this._ctx.arcTo(this._radius * 0.1, this._radius * -0.2, this._radius * 0.1, this._radius * -0.1, this._radius * 0.1);
+        this._ctx.lineTo(0, 0);
+        this._ctx.fillStyle = this._config.color.cursor;
+        this._ctx.fill();
+        this._ctx.rotate(-angle);
+        this._ctx.translate(-this._centerX + this._radius * -0.8 * Math.sin(angle), -this._centerY + this._radius * 0.8 * Math.cos(angle));
+    }
+
+    _drawFace() {
+        this._ctx.beginPath();
+        this._ctx.arc(this._centerX, this._centerY, this._radius * 1.1, 0, 2 * Math.PI);
+        this._ctx.fillStyle = this._config.color.face;
+        this._ctx.fill();
+    }
+
+    _drawNumbers() {
+        if (this._config.timeMax < 720) {
+            return;
+        }
+
+        const step = this._config.timeMax / 60 / 12;
+
+        this._ctx.font = `bold ${this._radius * (this._config.timeMax > 3600 ? 0.1 : 0.15)}px ${this._config.font}`;
+        this._ctx.textBaseline = 'middle';
+        this._ctx.textAlign = 'center';
+
+        for (let i = 0; i < 12; i++) {
+            const angle = i * Math.PI / -6;
+            const label = `${Math.floor(i * step)}`;
+
+            this._ctx.fillStyle = [1, 2, 6, 10, 11].includes(i) ? this._config.color.face : this._config.color.scale;
+
+            this._ctx.translate(this._centerX, this._centerY);
+
+            this._ctx.beginPath();
+            this._ctx.rotate(angle);
+            this._ctx.translate(0, this._radius * -0.925);
+            this._ctx.rotate(-angle);
+            this._ctx.fillText(label, 0, 0);
+            this._ctx.rotate(angle);
+            this._ctx.translate(0, this._radius * 0.925);
+            this._ctx.rotate(-angle);
+
+            this._ctx.translate(-this._centerX, -this._centerY);
+        }
+    }
+
+    _drawPanda() {
+        this._ctx.fillStyle = this._config.color.panda;
+
+        this._ctx.translate(this._centerX, this._centerY);
+
+        this._ctx.beginPath();
+        this._ctx.rotate(-0.25 * Math.PI);
+        this._ctx.arc(0, this._radius * -0.95, this._radius * 0.4, 0.875 * Math.PI, 0.125 * Math.PI);
+        this._ctx.fill();
+        this._ctx.rotate(0.25 * Math.PI);
+
+        this._ctx.beginPath();
+        this._ctx.rotate(0.25 * Math.PI);
+        this._ctx.arc(0, this._radius * -0.95, this._radius * 0.4, 0.875 * Math.PI, 0.125 * Math.PI);
+        this._ctx.fill();
+        this._ctx.rotate(-0.25 * Math.PI);
+
+        this._ctx.beginPath();
+        this._ctx.ellipse(this._radius * -0.35, 0, this._radius * 0.25, this._radius * 0.4, 0.1 * Math.PI, 0, 2 * Math.PI);
+        this._ctx.ellipse(this._radius * 0.35, 0, this._radius * 0.25, this._radius * 0.4, -0.1 * Math.PI, 0, 2 * Math.PI);
+        this._ctx.fill();
+
+        this._ctx.beginPath();
+        this._ctx.arc(0, this._radius * 0.85, this._radius * 0.2, 1.925 * Math.PI, 1.075 * Math.PI);
+        this._ctx.fill();
+
+        this._ctx.translate(-this._centerX, -this._centerY);
+    }
+
+    _drawText() {
+        this._ctx.beginPath();
+        this._ctx.fillStyle = this._config.color.text;
+        this._ctx.textBaseline = 'middle';
+        this._ctx.textAlign = 'center';
+
+        if (this._cursorMoving) {
+            const hours = this._config.timeMax >= 3600 ? `${Math.floor(this._timeLeft / 3600)}h` : '';
+            const minutes = `${Math.floor(this._timeLeft % 3600 / 60)}m`.replace(/^([0-9]m)/, '0$1');
+            const seconds = `${this._timeLeft % 60}s`.replace(/^([0-9]s)/, '0$1');
+
+            this._ctx.font = `${this._radius * 0.06}px monospace`;
+
+            this._ctx.fillText(`${hours} ${minutes} ${seconds}`, this._centerX, this._centerY + this._radius * 0.45);
+
+        } else {
+            this._ctx.font = `${this._radius * 0.05}px ${this._config.font}`;
+
+            if (this._config.text.line1 && this._config.text.line2) {
+
+                this._ctx.fillText(this._config.text.line1, this._centerX, this._centerY + this._radius * 0.425);
+                this._ctx.fillText(this._config.text.line2, this._centerX, this._centerY + this._radius * 0.475);
+
+            } else {
+                this._ctx.fillText(this._config.text.line1 || this._config.text.line2, this._centerX, this._centerY + this._radius * 0.45);
+            }
+        }
+    }
+
+    _drawTicks() {
+        for (let i = 0; i < 30; i++) {
+            const angle = i * Math.PI / 30;
+            const tickBegin = this._radius * (i % 5 ? 0.7 : 0.6);
+            const tickEnd = this._radius * 0.8;
+            const width = i % 5 ? 1 : 3;
+
+            this._ctx.translate(this._centerX, this._centerY);
+
+            this._ctx.beginPath();
+            this._ctx.moveTo(0, 0);
+            this._ctx.rotate(angle);
+            this._ctx.moveTo(0, tickBegin);
+            this._ctx.lineTo(0, tickEnd);
+            this._ctx.moveTo(0, -tickBegin);
+            this._ctx.lineTo(0, -tickEnd);
+            this._ctx.lineWidth = width;
+            this._ctx.strokeStyle = this._config.color.scale;
+            this._ctx.stroke();
+            this._ctx.rotate(-angle);
+
+            this._ctx.translate(-this._centerX, -this._centerY);
+        }
+    }
+
+    _drawTimeLeft() {
+        const angle = 2 * Math.PI / this._config.timeMax * (this._config.timeMax - this._timeLeft) - 0.5 * Math.PI;
+
+        this._ctx.translate(this._centerX, this._centerY);
+
+        this._ctx.fillStyle = this._config.color.timer;
+
+        this._ctx.beginPath();
+        this._ctx.moveTo(0, 0);
+        this._ctx.arc(0, 0, this._radius * 0.8, angle, 1.5 * Math.PI);
+        this._ctx.lineTo(0, 0);
+        this._ctx.fill();
+
+        this._ctx.beginPath();
+        this._ctx.arc(0, 0, this._radius * 0.025, 0, 2 * Math.PI);
+        this._ctx.shadowBlur = this._radius * 0.01;
+        this._ctx.shadowColor = '#333';
+        this._ctx.fill();
+        this._ctx.shadowBlur = 0;
+
+        this._ctx.translate(-this._centerX, -this._centerY);
+    }
+
+    _onMove(event) {
+        if (!this._cursorMoving) {
+            return;
+        }
+
+        const pageX = event.touches ? event.touches[0].pageX : event.pageX;
+        const pageY = event.touches ? event.touches[0].pageY : event.pageY;
+
+        const x = pageX - (event.target.clientLeft + event.target.offsetLeft) - this._centerX;
+        const y = pageY - (event.target.clientTop + event.target.offsetTop) - this._centerY;
+
+        let angle = Math.atan2(y, x) + 0.5 * Math.PI;
+
+        if (angle < 0) {
+            angle += 2 * Math.PI
+        }
+
+        const timeLeft = Math.round(this._config.timeMax - (this._config.timeMax / (2 * Math.PI) * angle));
+
+        if (Math.abs(timeLeft - this._cursorPrevious) < this._config.timeMax / 2) {
+            this._cursorPrevious = this._timeLeft;
+
+            this._timeLeft = timeLeft;
+
+            this._draw();
+        }
+    }
+
+    _onRelease() {
+        if (!this._cursorMoving) {
+            return;
+        }
+
+        this._cursorMoving = false;
+
+        this._draw();
+
+        this.start();
+    }
+
+    _onTouch(event) {
+        const angle = 2 * Math.PI / this._config.timeMax * (this._config.timeMax - this._timeLeft) - 0.5 * Math.PI;
+
+        const cx = this._centerX + this._radius * 0.925 * Math.cos(angle);
+        const cy = this._centerY + this._radius * 0.925 * Math.sin(angle);
+
+        const pageX = event.touches ? event.touches[0].pageX : event.pageX;
+        const pageY = event.touches ? event.touches[0].pageY : event.pageY;
+
+        const x = pageX - (event.target.clientLeft + event.target.offsetLeft);
+        const y = pageY - (event.target.clientTop + event.target.offsetTop);
+
+        this._cursorMoving = Math.sqrt((cx - x) ** 2 + (cy - y) ** 2) <= this._radius * 0.15;
+
+        if (this._cursorMoving) {
+            event.preventDefault();
+
+            this._cursorPrevious = this._timeLeft;
+        }
+    }
+
+    _playCompleted() {
+        if (this._audioContext) {
+            this._audioContext.resume().then(() => {
+                const gn = this._audioContext.createGain();
+                const osc1 = this._audioContext.createOscillator();
+                const osc2 = this._audioContext.createOscillator();
+
+                gn.gain.value = 1;
+                gn.connect(this._audioContext.destination);
+
+                osc1.frequency.value = 2050;
+                osc1.connect(gn);
+                osc1.start(this._audioContext.currentTime);
+                osc1.stop(this._audioContext.currentTime + 0.125);
+
+                osc2.frequency.value = 2050;
+                osc2.connect(gn);
+                osc2.start(this._audioContext.currentTime + 0.25);
+                osc2.stop(this._audioContext.currentTime + 0.35);
+            });
+        }
+    }
+
+    _playReminder() {
+        if (this._audioContext) {
+            this._audioContext.resume().then(() => {
+                const osc = this._audioContext.createOscillator();
+                const gn = this._audioContext.createGain();
+
+                gn.gain.value = 0.5;
+                gn.connect(this._audioContext.destination);
+
+                osc.frequency.value = 1850;
+                osc.connect(gn);
+                osc.start(this._audioContext.currentTime);
+                osc.stop(this._audioContext.currentTime + 0.125);
+            });
+        }
+    }
+
+    _resize() {
+        if (this._responsive) {
+            this._canvas.height = this._canvas.width = Math.min(this._canvas.parentElement.clientHeight, this._canvas.parentElement.clientWidth);
+        }
+    }
+
+    _tick() {
+        this._timeLeft -= (new Date().getTime() - this._lastTime.getTime()) / 1000;
+
+        const roundedTimeLeft = Math.round(this._timeLeft);
+
+        this._subscriptions.forEach((subscription) => {
+            if (roundedTimeLeft === subscription._timeLeft) {
+                subscription._callback(roundedTimeLeft);
+            }
+        });
+    }
+}
+
+PandaTimer.defaultConfig = {
+    alterable: true,
+    autostart: true,
+    color: {
+        background: '#fff',
+        cursor: 'rgba(255, 0, 0, 0.3)',
+        face: '#fff',
+        panda: '#333',
+        scale: '#333',
+        text: '#333',
+        timer: '#f00',
+    },
+    font: 'arial',
+    text: {
+        line1: 'Panda Timer',
+        line2: '',
+    },
+    timeLeft: 0,
+    timeMax: 3600,
+};
+
+if (window !== undefined) {
+    window.PandaTimer = PandaTimer;
+}
